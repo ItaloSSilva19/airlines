6cbcbe851564cc4ad7582e2b4038cda6939b9e86
ItaloSSilva19 <48248507+ItaloSSilva19@users.noreply.github.com>
2020-10-20 18:00:00 -0300

    create users views

--- airlines/users/views.py
+++ airlines/users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    return None
+
+
+def login_view():
+    return None
+
+
+def logout_view():
+    return None
