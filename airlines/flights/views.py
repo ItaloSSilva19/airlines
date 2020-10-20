2f49246d2efafb002ff5eebd8ecb7eb94c21e2a5
ItaloSSilva19 <48248507+ItaloSSilva19@users.noreply.github.com>
2020-10-20 00:37:03 -0300

    Create a new page, to each flight, that shows the properties of the flight by id

--- airlines/flights/views.py
+++ airlines/flights/views.py
@@ -7,3 +7,10 @@ def index(request):
     return render(request, "flights/index.html", {
         "flights": flights.objects.all()
     })
+
+
+def flight(request, flight_id):
+    flight = flights.objects.get(pk=flight_id)
+    return render(request, "flights/flight.html", {
+        "flight": flight
+    })
