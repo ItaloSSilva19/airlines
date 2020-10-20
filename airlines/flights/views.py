b3cadaf126ede620679fcf5be80b4b159ad4fb72
ItaloSSilva19 <48248507+ItaloSSilva19@users.noreply.github.com>
2020-10-19 23:55:01 -0300

    create a template and a functions that returns a list with the flights

--- airlines/flights/views.py
+++ airlines/flights/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import flights
+
 
 # Create your views here.
+def index(request):
+    return render(request, "flights/index.html", {
+        "flights": flights.objects.all()
+    })
